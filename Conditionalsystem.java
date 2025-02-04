af5b2b54318b6f3e24ac2122d1ecd4318b04fb70
shushanth2003 <bsshu2003@gmail.com>
2025-02-04 14:24:11 +0530

    !Fix:uploading

--- Conditionalsystem.java
+++ Conditionalsystem.java
@@ -1,15 +1,20 @@
 class Conditionalsystem {
     int userguess;
     int randomnum;
-    Conditionalsystem(int userguess, int randomnum){
+    boolean correctguess;
+    Conditionalsystem(int userguess, int randomnum,boolean correctguess){
         this.userguess=userguess;
         this.randomnum=randomnum;
+        this.correctguess=correctguess;
+    }
+    void setUserguessperformance(){
         if(userguess<randomnum){
             System.out.println("Too Low ! Try again.");
         }else if(userguess>randomnum){
             System.out.println("Too high! Try again.");
         }else{
             System.out.println("Congratutions! You guessed the number.");
+            correctguess=false;
         }
     }
 }
--- Main.java
+++ Main.java
@@ -1,15 +1,26 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int userguess=0;
+        int numberofattempts=0;
         Randomnum randnum1 = new Randomnum();
         int randnum=randnum1.getRandnum();
-        while(userguess!=randnum){
-            System.out.print("Guess the Number : ");
-            userguess=in.nextInt();
-            Conditionalsystem conditionalsystem = new Conditionalsystem(userguess,randnum);
+        boolean correctGuess=true;
+        try{
+            while(correctGuess){
+                System.out.print("Guess the Number : ");
+                userguess=in.nextInt();
+                Conditionalsystem conditionalsystem = new Conditionalsystem(userguess,randnum,correctGuess);
+                conditionalsystem.setUserguessperformance();
+                numberofattempts+=1;
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter an integer.");
         }
+
+        System.out.println("Number of attempts : "+numberofattempts);
     }
 }
